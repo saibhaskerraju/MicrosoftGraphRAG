b9acf340a3c543ad2b19f8b4daed741a4e7ccaf5
Sai Bhasker Raju <saibhaskerraju@outlook.com>
2025-03-21 20:40:39 -0400

    adding ruff to VS Code

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI
 import config
 import neo4j
 from neo4j_graphrag.llm import OpenAILLM as LLM
@@ -8,7 +8,6 @@ from neo4j_graphrag.retrievers import VectorRetriever
 from neo4j_graphrag.indexes import create_vector_index
 from neo4j_graphrag.generation.graphrag import GraphRAG
 from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
 
 
 app = FastAPI()
@@ -19,16 +18,18 @@ app.add_middleware(
     allow_methods=["GET"],
     allow_headers=["*"],
 )
-neo4j_driver = neo4j.GraphDatabase.driver(config.NEO4J_URI, auth=(config.NEO4J_USER, config.NEO4J_PASSWORD))
+neo4j_driver = neo4j.GraphDatabase.driver(
+    config.NEO4J_URI, auth=(config.NEO4J_USER, config.NEO4J_PASSWORD)
+)
 
-llm=LLM(
-   model_name=config.OPENAPI_MODEL,
-   model_params={
-       #"response_format": {"type": "json_object"},
-       "temperature": 0
-   },
-   api_key=config.OPENAPI_KEY,
-   base_url=config.OPENAPI_BASE_URL,
+llm = LLM(
+    model_name=config.OPENAPI_MODEL,
+    model_params={
+        # "response_format": {"type": "json_object"},
+        "temperature": 0
+    },
+    api_key=config.OPENAPI_KEY,
+    base_url=config.OPENAPI_BASE_URL,
 )
 
 embedder = Embeddings(
@@ -37,17 +38,18 @@ embedder = Embeddings(
     base_url=config.OPENAPI_BASE_URL,
 )
 
+
 # def extract_text_from_pdf(pdf_path):
-#     # Open the PDF file
-#     pdf_document = fitz.open(pdf_path)
+#      # Open the PDF file
+#      pdf_document = fitz.open(pdf_path)
 
-#     text = ""
-#     # Iterate through each page
-#     for page_number in range(len(pdf_document)):
-#         page = pdf_document.load_page(page_number)
-#         text += page.get_text()
+#       text = ""
+#        # Iterate through each page
+#        for page_number in range(len(pdf_document)):
+#             page = pdf_document.load_page(page_number)
+#             text += page.get_text()
 
-#     return text
+#         return text
 
 
 @app.get("/")
@@ -64,24 +66,28 @@ async def root():
         embedder=embedder,
         from_pdf=True,
     )
-    await kg_builder_pdf.run_async(file_path='sample.pdf')
-    create_vector_index(neo4j_driver, name="text_embeddings", label="Chunk",
-                   embedding_property="embedding", dimensions=1536, similarity_fn="cosine")
+    await kg_builder_pdf.run_async(file_path="sample.pdf")
+    create_vector_index(
+        neo4j_driver,
+        name="text_embeddings",
+        label="Chunk",
+        embedding_property="embedding",
+        dimensions=1536,
+        similarity_fn="cosine",
+    )
     # 2. KG Retriever
     vector_retriever = VectorRetriever(
-        neo4j_driver,
-        index_name="text_embeddings",
-        embedder=embedder
+        neo4j_driver, index_name="text_embeddings", embedder=embedder
     )
 
     # 3. GraphRAG Class
     rag = GraphRAG(llm=llm, retriever=vector_retriever)
 
     # 4. Run
-    response = rag.search( "List all companies bhasker worked in?")
+    response = rag.search("List all companies bhasker worked in?")
     print(response.answer)
-    
-    return {"pdf_text":  response.answer}
+
+    return {"pdf_text": response.answer}
 
 
 # if __name__ == "__main__":
